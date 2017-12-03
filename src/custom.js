919e4efa845145796157b43c7f9fc2b27def9534
likelionwonho <rhwhdeodhkd@naver.com>
2017-12-03 23:44:40 +0000

    171204 1st published / not responsive

---
+++ src/custom.js
@@ -0,0 +1,41 @@
+$('.navbar-nav > li > a').click(function(event){
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    event.preventDefault();
+});
+
+$(window).on('scroll', function(){
+	  
+});
+
+//Show totop btn on Scroll
+$(window).scroll(function () {
+  $('.to-top-btn').stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, 80);
+  if ($(this).scrollTop() > 400) {
+	  $('.to-top-btn').addClass('showme');
+  } else {
+	  $('.to-top-btn').removeClass('showme');
+  }
+});
+
+$('.to-top-btn').click(function() {
+    $('html, body').animate({scrollTop: '0'}, 600);
+});
+
+
+// fullpage js
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+	    menu: '#activityNav',
+	    loopBottom: false,
+		loopTop: false,
+		anchors: ['firstSect', 'secondSect', 'thirdSect', 'fourthSect']
+	});
+});
+
+var viewer01 = new Viewer(document.getElementById('activityAlbum01'));
+var viewer02 = new Viewer(document.getElementById('activityAlbum02'));
+var viewer03 = new Viewer(document.getElementById('activityAlbum03'));
+var viewer04 = new Viewer(document.getElementById('activityAlbum04'));
